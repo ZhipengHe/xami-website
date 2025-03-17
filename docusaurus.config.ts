99469f10cc890998be074c090bd30110ba09323c
Zhipeng He <me.zhipeng.he@gmail.com>
2025-03-17 14:53:09 +1000

    update production url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://xami-lab.org",
+  url: "https://preview.xami-lab.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: baseUrl,
